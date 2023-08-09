f2fa823536b2bbbfb1dea5493c64d9d7bec00b98
JPBago <bagolon@gmail.com>
2023-08-09 19:29:41 -0300

    Prueba de JPB

---
+++ src/prueba/JPBago.java
@@ -0,0 +1,8 @@
+
+package prueba;
+
+public class JPBago {
+    public void JP(){
+        System.out.println("Hola !!!");
+    }
+}
--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -27,4 +27,3 @@ public class Prueba {
 
 }
 // HOLA AQUI ESTOY //
-
